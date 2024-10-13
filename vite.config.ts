d0178305964bca903d7333ef0d15cab574487586
joseph.feng <joseph.feng@cartrack.com>
2024-10-13 14:53:33 +0800

    add the missing files

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     rollupOptions: {
       input: {
-        main: 'index.html'
+        main: resolve(__dirname, "index.html")
       },
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]"
       }
     }
-  }
+  },
+  publicDir: "public"
 });
