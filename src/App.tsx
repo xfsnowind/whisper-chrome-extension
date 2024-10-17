84f3aaa3a6a2edef8b5e2e0125fc94200f26f7df
joseph.feng <joseph.feng@cartrack.com>
2024-10-17 23:34:05 +0800

    improve the UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { useTranscriber } from "./hooks/useTranscriber";
 import Progress from "./components/Progress";
 import FileTile from "./components/FileTile";
 import FolderIcon from "@/assets/folder-icon.svg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useSummarize from "./hooks/useSummarize";
 
-const IS_WEBGPU_AVAILABLE = !!navigator.gpu;
+const IS_WEBGPU_AVAILABLE = "gpu" in navigator && !!navigator.gpu;
 
 function App() {
+  const [audioData, setAudioData] = useState<
+    { decoded: AudioBuffer; fileName: string } | undefined
+  >(undefined);
+
   const {
     transcript,
     isBusy,
@@ -34,25 +38,39 @@ function App() {
     <div className="min-w-64 min-h-32 p-4 bg-white">
       <div className="flex flex-col items-center justify-between mb-4 ">
         {isModelFilesReady ? (
-          "Model files loaded"
+          <div className="flex flex-col items-center justify-between mb-4">
+            Model files loaded
+            <FileTile
+              iconStr={FolderIcon}
+              text="From file"
+              onFileUpdate={(props) => {
+                setAudioData(props);
+              }}
+            />
+            {audioData && (
+              <div className="flex flex-col items-center justify-between mb-4">
+                File Name: {audioData.fileName}
+                <button
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 my-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+                  onClick={() => start(audioData.decoded)}
+                >
+                  Transcribe
+                </button>
+              </div>
+            )}
+          </div>
         ) : (
           <button
-            className="flex items-center justify-center rounded-lg p-2 bg-blue text-slate-500 hover:text-indigo-600 hover:bg-indigo-50 transition-all duration-200"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
             onClick={initialize}
           >
             Load Models
           </button>
         )}
-        <FileTile
-          iconStr={FolderIcon}
-          text="From file"
-          onFileUpdate={(decoded) => {
-            start(decoded);
-          }}
-        />
 
         {progressItems.length > 0 && (
           <div className="relative z-10 p-4 w-full text-center">
+            <VerticalBar />
             <label>Loading model files... (only run once)</label>
             {progressItems.map((data) => (
               <div key={data.file}>
@@ -73,3 +91,7 @@ function App() {
 }
 
 export default App;
+
+function VerticalBar() {
+  return <div className="w-[1px] bg-slate-200"></div>;
+}
--- src/components/FileTile.tsx
+++ src/components/FileTile.tsx
@@ -1,7 +1,12 @@
 export default function FileTile(props: {
   iconStr: string;
   text: string;
-  onFileUpdate: (decoded: AudioBuffer, blobUrl: string, mimeType: string) => void;
+  onFileUpdate: (props: {
+    decoded: AudioBuffer;
+    urlObj: string;
+    mimeType: string;
+    fileName: string;
+  }) => void;
 }) {
   // Create hidden input element
   const elem = document.createElement("input");
@@ -14,6 +19,8 @@ export default function FileTile(props: {
     // Create a blob that we can use as an src for our audio element
     const urlObj = URL.createObjectURL(files[0]);
     const mimeType = files[0].type;
+    console.log(files[0]);
+    const fileName = files[0].name;
 
     const reader = new FileReader();
     reader.addEventListener("load", async (e) => {
@@ -26,7 +33,7 @@ export default function FileTile(props: {
 
       const decoded = await audioCTX.decodeAudioData(arrayBuffer);
 
-      props.onFileUpdate(decoded, urlObj, mimeType);
+      props.onFileUpdate({ decoded, urlObj, mimeType, fileName });
     });
     reader.readAsArrayBuffer(files[0]);
 
@@ -47,12 +54,16 @@ function Tile(props: { iconStr: string; text?: string; onClick?: () => void }) {
   return (
     <button
       onClick={props.onClick}
-      className="flex items-center justify-center rounded-lg p-2 bg-blue text-slate-500 hover:text-indigo-600 hover:bg-indigo-50 transition-all duration-200"
+      className="flex items-center justify-center rounded-lg p-2 my-4 bg-blue text-slate-500 hover:text-indigo-600 hover:bg-indigo-50 transition-all duration-200"
     >
       <div className="w-7 h-7">
         <img src={props.iconStr} />
       </div>
-      {props.text && <div className="ml-2 break-text text-center text-md w-30">{props.text}</div>}
+      {props.text && (
+        <div className="ml-2 break-text text-center text-md w-30">
+          {props.text}
+        </div>
+      )}
     </button>
   );
 }
